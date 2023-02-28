3311511a7e2df6162839e1b10423af8578f88633
Caroline Frey <cfrey9@gmail.com>
2023-02-28 08:28:04 -0700

    Initial commit, remove storyboard

---
+++ WeatherAPI/WeatherAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAPI
+//
+//  Created by Caroline Frey on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
